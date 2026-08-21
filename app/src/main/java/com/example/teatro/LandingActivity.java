e3ed33781077a82ab76f4367ca8d7f408f050725
nacher1998 <nacher1998@gmail.com>
2026-08-21 20:29:20 +0200

    actualizacion

--- app/src/main/java/com/example/teatro/LandingActivity.java
+++ app/src/main/java/com/example/teatro/LandingActivity.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LandingActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
